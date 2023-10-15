7b21c837b56f8b4596406bd4e288be0f03267cb7
rishisingh34 <singh34rishi@gmail.com>
2023-10-15 23:50:27 +0530

    Invader Grid Created

---
+++ spaceInvader/music.js
@@ -0,0 +1,13 @@
+// const shootAudio = document.getElementById('shootAudio')
+
+// addEventListener('keydown', ({keyCode}) => {
+//     if ( keyCode == 32) {
+//         shootAudio.onload = function(){
+//             shootAudio.onplay()
+//         }
+//     }
+// })
+
+// addEventListener('keyup', ({keyCode}) => {
+    
+// })
--- spaceInvader/spaceInvader.js
+++ spaceInvader/spaceInvader.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector('canvas')
 const cont = canvas.getContext('2d')
 
+
 canvas.width = innerWidth
 canvas.height = innerHeight
 
@@ -30,8 +31,6 @@ class spaceShip {
             x : 0 
         }
 
-        this.rotation = 0 
-
         const image = new Image()
         image.src = './assets/spSprite1.png'
 
@@ -42,8 +41,6 @@ class spaceShip {
     // *--- Drawing the spaceship ---*//
 
     draw(){
-        cont.fillStyle= 'black'
-        cont.fillRect(0, 0, canvas.width , canvas.height)
         cont.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
     }
 
@@ -64,16 +61,19 @@ class Projectile{
         this.position = position
         this.velocity = velocity 
 
-        this.radius = 8
+        // this.radius = 8
+        
+        const bubble = new Image()
+        bubble.src = './assets/shooterBall.png'
+        this.image = bubble 
+        this.width = 28 
+        this.height = 28
+       
     }
 
+
     draw() {
-        cont.beginPath()
-        
-        cont.arc(this.position.x , this.position.y, this.radius, 0, Math.PI * 2)
-        cont.fillStyle = 'yellow'
-        cont.fill()
-        cont.closePath()
+        cont.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
     }
 
     update() {
@@ -83,15 +83,84 @@ class Projectile{
     }
 }
 
+// * --- Creating Enemies ------ * // 
+class Enemy{
+    constructor({position}){
+        this.position = {
+            x : position.x ,
+            y : position.y
+        }
+
+        this.velocity = {
+            x : 5 ,
+            y : 0 
+        }
+
+        const image = new Image()
+        image.src = './assets/aliens.png'
+
+        this.image = image
+        this.width = 50
+        this.height = 50           
+    }
+    // *--- Drawing the spaceship ---*//
+
+    draw(){
+        cont.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+    }
+
+    // *--- Drawing With updated Co-ordinates ---*// 
+    update({velocity}){
+        if(this.image){
+            this.draw()
+            this.position.x += this.velocity.x 
+            this.position.y += this.velocity.y 
+        }
+    }
+}
+
+class Grid {
+    constructor(){
+        this.position = {
+            x: 0 ,
+            y : 0 
+        }
+        this.velocity = {
+            x : 0 ,
+            y : 0 
+        }
+
+        this.enemies = []
+        const rows = Math.floor(Math.random() * 5  + 1 )
+        const columns = Math.floor(Math.random()*5 + 3)
+        for ( let i =1 ; i<= columns ; i++ ){
+           for( let j =0 ; j < rows ; j++ ){
+            this.enemies.push(new Enemy({
+                position : {
+                    x : i * 50 ,
+                    y : j * 50 
+                }
+            }))
+           }
+        }
+    }
+
+    update() {}
+}
+
+
+
 // *-- instantiating and animating spaceship, projectiles, enemies grid  ----* // 
 const sp = new spaceShip() 
 // *--- instantiating projectiles -----* // 
 const projectiles = []
+const grids = [new Grid()]
+
 
 function animate(){
     requestAnimationFrame(animate)
     sp.update()
-
+    
     // *--- SpaceShip Horizontal movement ------*//
     if( keys.right.pressed && sp.position.x + sp.width <= canvas.width){
         sp.velocity.x = 4 
@@ -102,13 +171,26 @@ function animate(){
     }
 
     //* ---- projectiles ----* // 
-    projectiles.forEach( projectile => {
-        projectile.update()
+    projectiles.forEach( (projectile, index) => {
+        if ( projectile.position.y === 0){
+            projectiles.splice(index, 1)
+        }
+        else {
+            projectile.update()
+        } 
+    })
+
+    grids.forEach((Grid) => {
+        Grid.update()
+        Grid.enemies.forEach(enemy => {
+            enemy.update({
+                velocity : Grid.velocity
+            })
+        })
     })
 
 }
 animate() 
-
 
 
 
@@ -127,7 +209,7 @@ addEventListener('keydown', ({keyCode}) => {
         case 32 : projectiles.push(
             new Projectile({
                 position : {
-                    x : sp.position.x + sp.width/2,
+                    x : sp.position.x + sp.width/2 - 13,
                     y : sp.position.y
                 },
                 velocity : {
@@ -149,9 +231,10 @@ addEventListener('keyup', ({keyCode}) => {
         case 68 : keys.right.pressed = false  
                 break ;
         case 39 : keys.right.pressed = false 
-                break ;  
+                break ; 
     }
 })
+
 
 
 
